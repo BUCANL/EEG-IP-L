b054b88e2c099981986861b50166fe8d25bfc731
James Desjardins <jdesjard@sharcnet.ca>
2018-05-14 15:31:57 -0400

    modify bids_init to be derivative compliant and add optional inputs for populating eeg sidecar json files.

--- bids/code/bids_init/mk_sidecar_eeg.m
+++ bids/code/bids_init/mk_sidecar_eeg.m
@@ -1,7 +1,11 @@
-function sidecar_eeg=mk_sidecar_eeg(EEG,fname)
+function sidecar_eeg=mk_sidecar_eeg(EEG,fname,varargin)
+
+opts=struct(varargin{:});
+
 
 % load current file only if it hasn't been loaded already
-if ~exist('EEG','var') || (exist('EEG','var') && isempty(EEG)) || ...
+if ~exist('EEG','var') || ...
+        (exist('EEG','var') && isempty(EEG)) || ...
   (~isempty(EEG) && ~strcmp(fname,[EEG.filepath '/' EEG.filename]));
     EEG = pop_loadset('filename',fname);
 end
@@ -9,38 +13,81 @@ end
 [path,fname,ext] = fileparts(fname);
 disp(['Creating ' fname '.json file...']);
 
-sidecar_eeg.TaskName='all';
-sidecar_eeg.TaskDescription='';
-sidecar_eeg.Instructions='';
-sidecar_eeg.CogAltasID='';
-sidecar_eeg.CogPOID='';
-sidecar_eeg.InstitutionName='';
-sidecar_eeg.InstitutionAddress='';
-sidecar_eeg.DeviceSerialNumber='';
 
-sidecar_eeg.EEGSamplingFrequency=EEG.srate;
-sidecar_eeg.ManufacturersAmplifierModelName='';
-sidecar_eeg.ManufacturerCapModelName='';
+if isfield(opts,'TaskName');
+    sidecar_eeg.TaskName=opts.TaskName;
+end
+if isfield(opts,'TaskDescription');
+    sidecar_eeg.TaskDescription=opts.TaskDescription;
+end
+if isfield(opts,'Instructions');
+    sidecar_eeg.Instructions=opts.Instructions;
+end
+if isfield(opts,'CogAtlasID');
+    sidecar_eeg.CogAltasID=opts.CogAltasID;
+end
+if isfield(opts,'CogPOID');
+    sidecar_eeg.CogPOID=opts.CogPOID;
+end
+if isfield(opts,'InstitutionName');
+    sidecar_eeg.InstitutionName=opts.InstitutionName;
+end
+if isfield(opts,'InstitutionAddress');
+    sidecar_eeg.InstitutionAddress=opts.InstitutionAddress;
+end
+if isfield(opts,'DeviceSerialNumber');
+    sidecar_eeg.DeviceSerialNumber=opts.DeviceSerialNumber;
+end
+sidecar_eeg.SamplingFrequency=EEG.srate;
 sidecar_eeg.EEGChannelCount=EEG.nbchan;
 sidecar_eeg.EOGChannelCount='';
 sidecar_eeg.EMGChannelCount='';
+sidecar_eeg.EEGReference=EEG.ref;
 sidecar_eeg.MiscChannelCount='';
 sidecar_eeg.TriggerChannelCount='';
-sidecar_eeg.PowerLineFrequency='';
-sidecar_eeg.EEGPlacementScheme='';
-sidecar_eeg.EEGReference=EEG.ref;
-sidecar_eeg.HardwareFilters='';
-sidecar_eeg.SoftwareFilters='';
-sidecar_eeg.RecordingDuration=EEG.pnts*(1/EEG.srate);
+if isfield(opts,'PowerLineFrequency');
+    sidecar_eeg.PowerLineFrequency='';
+end
+if isfield(opts,'EEGPlacementScheme');
+    sidecar_eeg.EEGPlacementScheme='';
+end
+if isfield(opts,'Manufacturer');
+    sidecar_eeg.Manufacturer='';
+end
+if isfield(opts,'ManufacturerModelName');
+    sidecar_eeg.ManufacturerModelName='';
+end
+if isfield(opts,'CapManufacturer');
+    sidecar_eeg.CapManufacturer='';
+end
+if isfield(opts,'CapModelName');
+    sidecar_eeg.CapModelName='';
+end
+if isfield(opts,'HardwareFilters');
+    sidecar_eeg.HardwareFilters='';
+end
+if isfield(opts,'SoftwareFilters');
+    sidecar_eeg.SoftwareFilters='';
+end
+
 if EEG.trials==1;
     sidecar_eeg.RecordingType='continuous';
     sidecar_eeg.EpochLength='Inf';
+    sidecar_eeg.RecordingDuration=EEG.pnts*(1/EEG.srate);
 else
     sidecar_eeg.RecordingType='epoched';
-    sidecar_eeg.EpochLength=EEG.xmax-EEGxmin;
+    sidecar_eeg.EpochLength=EEG.xmax-EEG.xmin;
+    %to propose
+    sidecar_eeg.EpochCount=EEG.trials;
 end
-sidecar_eeg.DeviceSoftwareVersion='';
-sidecar_eeg.SubjectArtefactDescription='';
-sidecar_eeg.DigitizedLandmarks='';
-sidecar_eeg.DigitizedHeadPoints='';
-sidecar_eeg.DigitizedElectrodes='';
+
+if isfield(opts,'DeviceSoftwareVersion');
+    sidecar_eeg.DeviceSoftwareVersion='';
+end
+if isfield(opts,'SubjectArtefactDescription');
+    sidecar_eeg.SubjectArtefactDescription='';
+end
+%to propose
+if isfield(opts,'Category')
+    sidecar_eeg.Category=opts.Category;
+end
